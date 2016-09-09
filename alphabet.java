62d739afed0ed4e7c06bf787895acbbf97fb91c2
meenaramasamy3 <meenaswt3@gmail.com>
2016-09-09 20:43:40 +0530

    Update alphabet.java

--- alphabet.java
+++ alphabet.java
@@ -4,17 +4,17 @@ public class Alphabet
 {
     public static void main(String args[])
     {
-        char a;
+        char a1;
         Scanner scan = new Scanner(System.in);
-        ch = scan.next().charAt(0);
+        a1 = scan.next().charAt(0);
 		
-        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+        if((a1>='a' && ch<='z') || (a1>='A' && a1<='Z'))
         {
-            System.out.print(ch + " is an Alphabet");
+            System.out.print(a1 + " is an Alphabet");
         }
         else
         {
-            System.out.print(ch + " is not an Alphabet");
+            System.out.print(a1 + " is not an Alphabet");
         }
     }
 }
