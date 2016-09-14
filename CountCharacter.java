dffe91720d1a009707818b5e34927b01d836b121
meenaramasamy3 <meenaswt3@gmail.com>
2016-09-14 15:22:23 +0530

    Create CountCharacter.java

---
+++ CountCharacter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class CountCharacters {
+	public static void main(String args[])
+	{Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int count=s.length();
+		System.out.println(count);
+	}
+
+}
