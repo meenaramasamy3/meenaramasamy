4439f01d55b1a661a7585c4dc23d6d198f5a873f
meenaramasamy3 <meenaswt3@gmail.com>
2016-09-17 21:38:24 +0530

    Update and rename Power.java to Powerr.java

--- Powerr.java
+++ Powerr.java
@@ -3,11 +3,8 @@ public static void main(String angt[]){
 Scanner sc = new Scanner(System.in);
 int num1, num2;
 double num;
-System.out.println("Enter a number");
 num1=sc.nextInt();
-System.out.println("Enter a power of the number");
 num2=sc.nextInt();
-// Finding power of a number
 num=Math.pow(num1,num2);
 System.out.println(num);
 }
